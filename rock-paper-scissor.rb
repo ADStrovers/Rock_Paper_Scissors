c89d4fb739bf062d3bae09e82e49ee762a3fab3d
Andrew Strovers <ADStrovers@gmail.com>
2015-01-29 14:29:09 -0600

    Completed Level 0 Step 2.  Player Class with Method for driver.

--- rock-paper-scissor.rb
+++ rock-paper-scissor.rb
@@ -1,36 +1,53 @@
-# Basic rock-paper-scissors game (no custom objects). Output should be sensibly formatted. Should only accept valid moves. Players only need to play a single round.
+# Player classes + overall driver method. Driver method should create players, tell the players to prompt for moves, and determine the winner. Players should keep track of their names and moves, and should still only accept valid moves. From the user’s perspective, the game should appear to work exactly the same.
 
-valid_throws = {"Rock" => 1, "Paper" => 3, "Scissors" => 7}
-p1_throw = ""
-p2_throw = ""
-while valid_throws.include?(p1_throw) == false
-  print "Player 1, please give me a valid throw: "
-  p1_throw = gets.chomp.capitalize
-end
-while valid_throws.include?(p2_throw) == false
-  print "Player 2, please give me a valid throw: "
-  p2_throw = gets.chomp.capitalize
-end
-result = valid_throws[p1_throw] + valid_throws[p2_throw]
-case result
-when 2, 6, 14
-  puts "Round ended in a tie."
-when 4
-  if p1_throw == "Rock"
-    puts "Congratulations Player 1! You win!"
-  else
-    puts "Congratulations Player 2! You win!"
+require 'pry'
+
+def start_rps(name1 = "Player 1", name2 = "Player 2")
+  valid_moves = {"Rock" => 1, "Paper" => 3, "Scissors" => 7}
+  player1 = Player.new(name1)
+  player2 = Player.new(name2)
+  while valid_moves.include?(player1.move) == false
+    print "Player 1, please give me a valid move: "
+    player1.move = gets.chomp.capitalize
   end
-when 10
-  if p1_throw == "Scissors"
-    puts "Congratulations Player 1! You win!"
-  else
-    puts "Congratulations Player 2! You win!"
+  while valid_moves.include?(player2.move) == false
+    print "Player 2, please give me a valid move: "
+    player2.move = gets.chomp.capitalize
   end
-when 8
-  if p1_throw == "Paper"
-    puts "Congratulations Player 1! You win!"
-  else
-    puts "Congratulations Player 2! You win!"
+  result = valid_moves[player1.move] + valid_moves[player2.move]
+  case result
+  when 2, 6, 14
+    puts "Round ended in a tie."
+  when 4
+    if player1.move == "Rock"
+      puts "Congratulations Player 1! You win!"
+    else
+      puts "Congratulations Player 2! You win!"
+    end
+  when 10
+    if player1.move == "Scissors"
+      puts "Congratulations Player 1! You win!"
+    else
+      puts "Congratulations Player 2! You win!"
+    end
+  when 8
+    if player1.move == "Paper"
+      puts "Congratulations Player 1! You win!"
+    else
+      puts "Congratulations Player 2! You win!"
+    end
   end
 end
+
+
+class Player
+  attr_reader :name
+  attr_accessor :move
+  
+  def initialize(name)
+    @name = name
+    @move = ""
+  end
+end
+
+binding.pry
